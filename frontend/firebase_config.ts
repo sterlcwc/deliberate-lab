1de3f4b893a61513916d8a7cfbfb553a1b27f1d6
sterlcwc <scw222@cornell.edu>
2024-11-29 22:55:01 -0500

    new firebase config

---
+++ frontend/firebase_config.ts
@@ -0,0 +1,10 @@
+import {FirebaseOptions} from 'firebase/app';
+
+export const FIREBASE_CONFIG: FirebaseOptions = {
+  apiKey: 'AIzaSyBgnbyDmnTpWbsU8lS7j4KfRxbobH5SwTg',
+  authDomain: 'deliberate-a9d93.firebaseapp.com',
+  projectId: 'deliberate-a9d93',
+  storageBucket: 'deliberate-a9d93.firebasestorage.app',
+  messagingSenderId: '146871721365',
+  appId: '1:146871721365:web:8cbfc876de5fbe9d602a9c',
+};
